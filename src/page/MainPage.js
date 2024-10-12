1783e99514a234f06535bde8c1a48cc453cfe23a
Sheeba Sharon <sheebasharon@Sheebas-MacBook-Air.local>
2024-10-12 19:37:29 +0530

    added the main page

---
+++ src/page/MainPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const MainPage =()=> {
+    return (
+        <React.Fragment>
+            <section>
+            <div className='wrapper'>
+               <h1>BYTES AND EATS</h1>
+               <button className='button'>Start</button>
+                </div>
+            </section>
+                 
+        </React.Fragment>
+       
+    )
+}
+
+export default MainPage
